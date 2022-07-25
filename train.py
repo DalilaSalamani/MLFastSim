c9a37841517d632cf9491a6ea28dd4567fdf4c1e
Maciek Draguła <maciekdragula.reklamy@gmail.com>
2022-07-25 10:51:08 +0200

    Distributed training.
    Now both the training and the hyperparameter optimization may be performed on multiple GPUs within one machine. The strategy which is used for a distributed training is MirroredStrategy. Also implemented GPULimiter which allows to specify which GPUs should be used during the training and what is the maximum memory limit that TF can allocate for a training.

--- train.py
+++ train.py
@@ -1,21 +1,50 @@
-from core.model import VAEHandler
+from argparse import ArgumentParser
+
+from core.constants import GPU_IDS, MAX_GPU_MEMORY_ALLOCATION
+from utils.gpu_limiter import GPULimiter
 from utils.preprocess import preprocess
 
-# 1. Data loading/preprocessing
 
-# The preprocess function reads the data and performs preprocessing and encoding for the values of energy,
-# angle and geometry
-energies_train, cond_e_train, cond_angle_train, cond_geo_train = preprocess()
+def parse_args():
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("--max-gpu-memory-allocation", type=int, default=MAX_GPU_MEMORY_ALLOCATION)
+    argument_parser.add_argument("--gpu-ids", type=str, default=GPU_IDS)
+    args = argument_parser.parse_args()
+    return args
 
-# 2. Model architecture
-vae = VAEHandler()
 
-# 3. Model training
-histories = vae.train(energies_train,
-                      cond_e_train,
-                      cond_angle_train,
-                      cond_geo_train
-                      )
+def main():
+    # 0. Parse arguments.
+    args = parse_args()
+    max_gpu_memory_allocation = args.max_gpu_memory_allocation
+    gpu_ids = args.gpu_ids
 
-# Note : One history object can be used to plot the loss evaluation as function of the epochs. Remember that the
-# function returns a list of those objects. Each of them represents a different fold of cross validation.
+    # 1. Set GPU memory limits.
+    GPULimiter(_gpu_ids=gpu_ids, _max_gpu_memory_allocation=max_gpu_memory_allocation)()
+
+    # 2. Data loading/preprocessing
+
+    # The preprocess function reads the data and performs preprocessing and encoding for the values of energy,
+    # angle and geometry
+    energies_train, cond_e_train, cond_angle_train, cond_geo_train = preprocess()
+
+    # 3. Manufacture model handler.
+
+    # This import must be local because otherwise it is impossible to call GPULimiter.
+    from core.model import VAEHandler
+    vae = VAEHandler(_number_of_k_fold_splits=1, _epochs=50, _learning_rate=0.01, _batch_size_per_replica=128,
+                     _early_stop=False)
+
+    # 4. Train model.
+    histories = vae.train(energies_train,
+                          cond_e_train,
+                          cond_angle_train,
+                          cond_geo_train
+                          )
+
+    # Note : One history object can be used to plot the loss evaluation as function of the epochs. Remember that the
+    # function returns a list of those objects. Each of them represents a different fold of cross validation.
+
+
+if __name__ == "__main__":
+    exit(main())

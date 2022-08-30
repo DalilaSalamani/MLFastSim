5b8bef0785301b6b01c15d4cad319a61e7d111d8
Piyush Raikwar <piyushraikwar555@gmail.com>
2022-08-30 18:06:16 +0200

    yapf formatting

--- convert.py
+++ convert.py
@@ -12,7 +12,6 @@ from onnxruntime import InferenceSession
 
 from core.constants import GLOBAL_CHECKPOINT_DIR, CONV_DIR, ORIGINAL_DIM
 from core.model import VAEHandler
-
 """
     epoch: epoch of the saved checkpoint model
     study-name: study-name for which the model is trained for
@@ -40,13 +39,16 @@ def main(argv):
 
     # Load the saved weights
     weights_dir = f"VAE_epoch_{epoch:03}" if epoch is not None else "VAE_best"
-    vae.model.load_weights(f"{GLOBAL_CHECKPOINT_DIR}/{study_name}/{weights_dir}/model_weights").expect_partial()
+    vae.model.load_weights(
+        f"{GLOBAL_CHECKPOINT_DIR}/{study_name}/{weights_dir}/model_weights"
+    ).expect_partial()
 
     # 2. Convert the model to ONNX format
     # Create the Keras model, convert it into an ONNX model, and save.
     keras_model = vae.model.decoder
     output_path = f"{CONV_DIR}/{study_name}/Generator_{weights_dir}.onnx"
-    onnx_model = tf2onnx.convert.from_keras(keras_model, output_path=output_path)
+    onnx_model = tf2onnx.convert.from_keras(keras_model,
+                                            output_path=output_path)
 
     # Checking the converted model
     input_1 = np.random.randn(10).astype(np.float32).reshape(1, -1)
@@ -56,7 +58,13 @@ def main(argv):
 
     sess = InferenceSession(output_path)
     # TODO: @Piyush-555 Find a way to use predefined names
-    result = sess.run(None, {'input_9': input_1, 'input_6': input_2, 'input_7': input_3, 'input_8': input_4})
+    result = sess.run(
+        None, {
+            'input_9': input_1,
+            'input_6': input_2,
+            'input_7': input_3,
+            'input_8': input_4
+        })
     assert result[0].shape[1] == ORIGINAL_DIM
 
 
